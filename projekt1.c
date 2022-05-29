de92ddc78370a220671a96e19b191e6e609f50d4
bpc <b.ciereszynski@wp.pl>
2022-05-30 00:44:13 +0200

    initial

---
+++ projekt1.c
@@ -0,0 +1,79 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define N 5
+#define constTime 1000
+
+pthread_mutex_t bridge[2];
+
+int cityStay[2]; //number of cars staying in cityX
+int cityWait[2]; //number of cars waiting to leave cityX
+
+void display(int carNumber, int from){
+	printf("A-%d %d>>>", cityStay[0], cityWait[0]);
+	switch(from)
+	{
+		case 0:
+		printf("[>> %d >>]", carNumber);
+		break;
+		case 1:
+		printf("[<< %d <<]", carNumber);
+		break;
+	}
+	printf("<<<<%d %d-B\n", cityWait[1], cityStay[1] );
+}
+
+int opposite(int city){
+	return (city+1)%2;
+}
+
+void *carFunction(void *arg){
+	int nr = (int)arg;
+    int city = rand()%2; // 0 - cityA, 1 - cityB
+	cityStay[city]++;
+
+	for(;;){
+		usleep(constTime+rand()%constTime*1000);	//car is staying in the city
+			cityStay[city]--;		//car signals need for leave and wait in queue
+			cityWait[city]++;		// *
+		pthread_mutex_lock(bridge+city);
+			cityWait[city]--;	//car leave city and enter the bridge
+			display(nr,city);	//car passes the bridge
+			usleep(constTime);	//*
+			city=opposite(city);//car appers on the other side
+			cityStay[city]++;	//*
+		if(cityWait[city]!=0||cityWait[opposite(city)]+cityStay[opposite(city)]==0){	
+			pthread_mutex_unlock(bridge+city); //unlock bridge for cars from this side
+		}
+		else 
+			pthread_mutex_unlock(bridge+opposite(city));	//unlock other side
+	}
+}
+
+
+int main(){
+
+    pthread_t cars[N];
+
+	srand(time(NULL));
+	pthread_mutex_init(bridge,NULL);
+	pthread_mutex_init(bridge+1,NULL);
+
+	pthread_mutex_lock(bridge+1);
+	pthread_mutex_lock(bridge);
+    for(int i =0; i<N; i++){
+        pthread_create(cars+i, NULL, carFunction, (void*)i);
+    }
+	if(cityStay[0]+cityWait[0])
+		pthread_mutex_unlock(bridge);
+	else
+		pthread_mutex_unlock(bridge+1);
+	
+
+	for(int i =0; i<N; i++){
+        pthread_join(cars[i], NULL);
+    }    
+}
